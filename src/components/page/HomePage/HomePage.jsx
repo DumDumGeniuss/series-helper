22a69ef669e79b8cbe8cee71ea31e8342cda9f84
DumDumGeniuss <messi_yang@syntrontech.com>
2017-01-06 15:25:42 +0800

    Compress all the images, and add some new images on Home page

--- src/components/page/HomePage/HomePage.jsx
+++ src/components/page/HomePage/HomePage.jsx
@@ -10,6 +10,8 @@ class HomePage extends React.Component {
 	render () {
 		const style = require('./HomePage.scss');
 		const functionIndicator1 = require('./functionIndicator1.jpg');
+		const functionIndicator2 = require('./functionIndicator2.jpg');
+		const functionIndicator3 = require('./functionIndicator3.jpg');
 		return (
 			<div className={style.homePageContainer}>
 				<div className={style.firstZone}>
@@ -20,13 +22,19 @@ class HomePage extends React.Component {
 					<h2 className={style.firstZoneSubTitle}>點擊圖示進入</h2>
 				</div>
 				<div className={style.secondZone}>
-					<h2 className={style.secondZoneTitle}>操作說明</h2>
+					<h2 className={style.secondZoneTitle}>操作介面</h2>
+					<h2 className={style.secondZoneSubTitle}>利用簡易的操作介面管理您所有的追劇進度</h2>
 					<div className={style.displayPhoto}>
 						<img src={functionIndicator1} />
 					</div>
-					<h2 className={style.secondZoneSubTitle}>利用簡易的操作介面管理您所有的追劇進度</h2>
 					<h2 className={style.secondZoneSubTitle}>亦可利用編輯新增其他網站<b>首頁</b>連結，方便您輕鬆追劇</h2>
+					<div className={style.displayPhoto}>
+						<img src={functionIndicator2} />
+					</div>
 					<h2 className={style.secondZoneSubTitle}>若您富有創意，您也可以紀錄影集以外的事務</h2>
+					<div className={style.displayPhoto}>
+						<img src={functionIndicator3} />
+					</div>
 					<h2 className={style.secondZoneSubTitle}>無需耗費時間做繁瑣的安裝及註冊程序</h2>
 					<h2 className={style.secondZoneSubTitle}>您只需要瀏覽器跟您的Facebook帳號即可快速登陸使用！</h2>
 				</div>
